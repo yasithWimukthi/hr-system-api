66708c2d33a5cb29ab96da27dcce55151caed029
yasith wimukthi <it19966922@my.sliit.lk>
2022-02-17 15:52:28 +0530

    create leave type controller

---
+++ database/migrations/2022_02_17_101608_create_leave_types_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeaveTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leave_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('country');
+            $table->string('leave_name');
+            $table->integer('number_of_days');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leave_types');
+    }
+}
