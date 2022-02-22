bedd0b0b9991c5195b9f6d4046fe6d451cfcaec3
yasith wimukthi <it19966922@my.sliit.lk>
2022-02-22 21:32:17 +0530

    update leave settings

--- app/Models/LeaveType.php
+++ app/Models/LeaveType.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LeaveType extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'country',
+        'leave_name',
+        'relevant_days',
+        'joined_month',];
 }
---
+++ database/migrations/2022_02_22_135123_create_leave_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeaveSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leave_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('country');
+            $table->string('leave_name');
+            $table->string('relevant_days');
+            $table->string('joined_month');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leave_settings');
+    }
+}
